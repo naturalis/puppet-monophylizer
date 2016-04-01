8d54a0d03c316d803810abef5a2ea86572ef8a04
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2016-04-01 12:59:35 +0000

    updated dependencies of various components

--- manifests/init.pp
+++ manifests/init.pp
@@ -31,13 +31,13 @@
 #
 #
 class monophylizer (
-  $instances           = {'monophylizer.cloud.naturalis.nl' => { 
-                           'serveraliases'   => '*.cloud.naturalis.nl',
+  $instances           = {'monophylizer.naturalis.nl' => { 
+                           'serveraliases'   => '*.naturalis.nl',
                            'docroot'         => '/var/www/monophylizer',
                            'port'            => 80,
                            'scriptalias'     => '/usr/lib/cgi-bin',
-                           'serveradmin'     => 'webmaster@monophylizer.naturalis.nl',
-                           'priority'        => 20,
+                           'serveradmin'     => 'aut@naturalis.nl',
+                           'priority'        => 10,
                            'options'         => '+FollowSymLinks +ExecCGI',
                           },
 			},
@@ -53,71 +53,75 @@ class monophylizer (
   # installs the command line tool to read perldoc-formatted manual pages
   # using whatever is the package manager (probably apt-get)
   package { 'perl-doc':
-    ensure => present,
+    ensure     => present,
+    require    => Package['build-essential']
   }
   
   # installs the XML::Twig perl library for parsing XML documents, needed
   # for monophylizing NeXML and phyloXML documents.
   package { 'libxml-twig-perl':
-    ensure => present,
+    ensure     => present,
+  }
+
+  # install build-essential requirement of cpanminus for installing modules
+  package { 'build-essential':
+    ensure     => present,
   }
 
   # sets up perl tools, e.g. cpanminus
   class { 'perl':
-    require  => Package['perl-doc'],
+    require    => Package['perl-doc'],
   }
 
   # installs the Bio::Phylo perl library using cpanminus
   perl::module { 'Bio::Phylo':
-    require  => Package['perl-doc'],
+    require    => Package['perl-doc'],
   }
 
   class { 'monophylizer::instances': 
-    instances => $instances,
+    instances  => $instances,
+  }
+
+  # install git, required for vcsrepo
+  package { 'git':
+    ensure     => present
   }
 
   # clones the monophylizer repository into /var
   vcsrepo { $appdir:
-    ensure   => latest,
-    provider => git,
-    source   => $reposource,
-    revision => 'master',
-    require  => Class['monophylizer::instances'],
+    ensure     => latest,
+    provider   => git,
+    source     => $reposource,
+    revision   => 'master',
+    require    => [Package['git'],Class['monophylizer::instances']],
   }
 
   # sets monophylizer.pl script to executable
   file { "${appdir}/script/monophylizer.pl":
-    ensure  => 'file',
-    mode    => '0755',
-    require => Vcsrepo[$appdir],
+    ensure     => 'file',
+    mode       => '0755',
+    require    => Vcsrepo[$appdir],
   }
 
   # creates executable link from cgi-bin to monophylizer.pl script
   file { "${libdir}/monophylizer.pl":
-    ensure  => 'link',
-    mode    => '0777',
-    target  => "${appdir}/script/monophylizer.pl",
-    require => Vcsrepo[$appdir],
+    ensure     => 'link',
+    mode       => '0777',
+    target     => "${appdir}/script/monophylizer.pl",
+    require    => Vcsrepo[$appdir],
   }
 
-#  file { "${webdir}/index.html":
-#    ensure  => 'link',
-#    mode    => '0644',
-#    target  => "${appdir}/html/index.html",
-#    require => Vcsrepo[$appdir],
-#  }
-
   exec { "static_files":
-    command => "/bin/cp -r * ${webdir}/",
-    creates => "${webdir}/monophylizer.html",
-    cwd     => "${appdir}/html/",
-    require => Vcsrepo[$appdir],
+    command    => "/bin/cp -r * ${webdir}/",
+    creates    => "${webdir}/monophylizer.html",
+    cwd        => "${appdir}/html/",
+    require    => Vcsrepo[$appdir],
   }
 
   file { "${webdir}/sorttable.js":
-    ensure  => 'link',
-    mode    => '0644',
-    target  => "${appdir}/script/sorttable.js",
-    require => Vcsrepo[$appdir],
+    ensure     => 'link',
+    mode       => '0644',
+    target     => "${appdir}/script/sorttable.js",
+    require    => Vcsrepo[$appdir],
   }
 }
